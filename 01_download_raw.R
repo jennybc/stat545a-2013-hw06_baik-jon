14dcbb041212d55f7d18539071662c31dd3518d7
Jonathan Baik <jonnybaik@gmail.com>
2013-10-19 18:21:33 -0700

    added function to download raw data, renamed files
    Since the data is larger than 100 MB, github doesn't allow us to push
    the data

---
+++ 01_download_raw.R
@@ -0,0 +1,29 @@
+# Function for downloading files from Dropbox
+# Adapted from http://thebiobucket.blogspot.ca/2013/04/download-files-from-dropbox.html
+dl_from_dropbox <- function(x, key, toFolder) {
+  require(RCurl)
+  bin <- getBinaryURL(paste0("https://dl.dropboxusercontent.com/u/", key, "/raw_data/", x),
+                      ssl.verifypeer = FALSE)
+  
+  if(!missing(toFolder)) setwd(toFolder)
+  
+  con <- file(x, open = "wb")
+  writeBin(bin, con)
+  close(con)
+  message(noquote(paste(x, "read into", getwd())))
+}
+
+# Run this to create a folder called "raw_data"
+# First set working directory!!
+dir.create("raw_data")
+
+# Now download the raw data!
+
+# The readme, ~4 KB
+dl_from_dropbox("readme.txt", "14072013", "raw_data/")
+# MYSQL data base dump, ~241 MB
+dl_from_dropbox("parkingtickets.sql", "14072013", "raw_data/") 
+# SQLite data base, ~188 MB
+dl_from_dropbox("parkingtickets.sqlite", "14072013", "raw_data/") 
+# Script to convert MYSQL data base dump to SQLite data base, ~2KB
+dl_from_dropbox("mysql2sqlite.sh", "14072013", "raw_data/") 
---
+++ 03_data_process.R
@@ -0,0 +1,4 @@
+# Load the RSQLite package
+library("RSQLite")
+
+# Open the connection to the SQLite data base
